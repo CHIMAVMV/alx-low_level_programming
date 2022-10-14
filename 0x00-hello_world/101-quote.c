86b933f40d79d20b2a77e4586e3f7a54db50c30f
Nweke Chima Gideon <nweke_chima@yahoo.com>
2022-10-13 18:57:49 -0700

    hello world task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - print UNIX OS
+ *
+ * Return: Always 1 ( UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
