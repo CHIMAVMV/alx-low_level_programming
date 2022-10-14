c49217dbecb7e236f9f8cf9caedce173583572c6
Nweke Chima Gideon <nweke_chima@yahoo.com>
2022-10-13 18:29:03 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point of execution
+ *
+ * Return:Always 0 (executing a successful name)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char. %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of an long int: %d byte(s)\n", sizeof(c));
+	printf("Size of an long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of an float: %d byte(s)\n", sizeof(e));
+	return (0);
+}
