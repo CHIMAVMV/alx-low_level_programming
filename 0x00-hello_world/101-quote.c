65833d4196ba2f829dfcaab2af0cb143004c2876
Nweke Chima Gideon <nweke_chima@yahoo.com>
2022-10-13 20:22:45 -0700

    fundamentals of C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main - print UNIX OS
+ * main - print UNIX O
  *
- * Return: Always 1 ( UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity)
+ * Return: Always 1 (system)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19"\n", 59);
 	return (1);
 }
